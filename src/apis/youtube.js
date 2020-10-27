71c2a02fc076f2bb7b32e41b635adaf37339edde
Brandan Sayarath <brandan@brandan.tech>
2020-10-27 16:00:47 -0700

    add youtube api config

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyDEr1JB4IrLSJHz4uJa7ERSigcnlLOuei8";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: "5",
+    key: KEY,
+  },
+});
