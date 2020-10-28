714162af5fb5ed225d521f4843b9da2a23c8cd77
Brandan Sayarath <brandan@brandan.tech>
2020-10-27 18:27:09 -0700

    add styling to video list and items

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,9 +3,9 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem />
-  })
-  return <div>{renderedList}</div>
-}
+    return <VideoItem video={video} />;
+  });
+  return <div className="ui relaxed divided list">{renderedList}</div>;
+};
 
 export default VideoList;
